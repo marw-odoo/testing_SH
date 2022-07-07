eee5507ac132867b9926cf188d2d3efe2aef46c5
Rwasenge <marw@odoo.com>
2022-07-07 14:57:43 +0000

    added acad

---
+++ odoo_acad/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Trianing application""",
+    
+    'description': ' has stuff in it to learn to',
+    
+    'author': 'Odoo',
+    
+    'website': 'https://odoo.com',
+    
+    'category': "Training",
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
