3fc618fe3b7fd9c4f94c51ad233e25476bdc2358
Rwasenge <marw@odoo.com>
2022-07-07 17:54:34 +0000

    init typo fix

--- odoo_acad/__manifest__.py
+++ odoo_acad/__manifest__.py
@@ -5,15 +5,17 @@
     
     'summary': """Astronaut Trianing application""",
     
-    'description': ' has stuff in it to learn to',
+    'description': 'has stuff in it to learn to',
     
     'author': 'Rwasenge',
     
     'website': 'https://odoo.com',
     
-    'category': "Training",
+    'category': 'Training',
     
     'version': '0.1',
+    
+    'license': 'OPL-1',
     
     'depends': ['base'],
     
---
+++ odoo_acad/models/spaceship.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Spaceship(models.Model):
+    
+    _name = 'ss.spaceship'
+    _description = 'hopefully this model has an FTL drive!'
